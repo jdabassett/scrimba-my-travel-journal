7559436b4d6de65e1c4c83b5e4854ba16fb7a2db
jdabassett <jacobbassett@gmail.com>
2023-01-18 20:04:40 -0700

    mostly set up with components and font awesome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
+import Navbar from './components/Navbar.jsx';
+import MainContents from './components/MainContents.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+export default function App() {
   return (
     <div className="app-container">
-      <p>boogers</p>
+      <Navbar />
+      <MainContents />
     </div>
   );
 }
 
-export default App;
