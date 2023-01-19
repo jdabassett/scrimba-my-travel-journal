8c70d3ef7c8e6c48cd2e5502fbad0224c58b12ad
jdabassett <jacobbassett@gmail.com>
2023-01-18 23:00:58 -0700

    minimum viable product

---
+++ src/data.js
@@ -0,0 +1,63 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {   
+      id:1,
+      title: "Mount Fuji",
+      location: "Japan",
+      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+      startDate: "01 Jan, 2025",
+      endDate: "02 Jan, 2025",
+      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists. Jacob hasn't been there yet.",
+      imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {    
+    id:2, 
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/2WMJX8n6ED2SPbTJ7",
+    startDate: "03 Jan, 2025",
+    endDate: "04 Jan, 2025",
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings. Jacob hasn't been there yet.",
+    imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+  },
+  {   
+    id:3,
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/8AhiLySpHrkiwfAW7",
+    startDate: "05 Jan, 2025",
+    endDate: "06 Jan, 2025",
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. Jacob hasn't been there yet.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+  },
+  {   
+    id:4,
+    title: "Testing if dynamic",
+    location: "Testing if dynamic",
+    googleMapsUrl: "NA",
+    startDate: "NA",
+    endDate: "NA",
+    description: "Testing if dynamic",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+},
+{    
+  id:5, 
+  title: "Testing if dynamic",
+  location: "Testing if dynamic",
+  googleMapsUrl: "NA",
+  startDate: "NA",
+  endDate: "NA",
+  description: "Testing if dynamic",
+  imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+},
+{   
+  id:6,
+  title: "Testing if dynamic",
+  location: "Testing if dynamic",
+  googleMapsUrl: "NA",
+  startDate: "NA",
+  endDate: "NA",
+  description: "Testing if dynamic",
+  imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+}
+]
